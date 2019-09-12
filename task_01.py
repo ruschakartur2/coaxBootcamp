a58b1536bf837561d3160bb032720aacbc9d7de8
ruschakartur2 <arturruschak1@gmail.com>
2019-09-12 22:06:41 +0300

    Upload finished files

---
+++ task_01.py
@@ -0,0 +1,8 @@
+
+def rev(s):
+    s = s[::-1]
+    return s
+
+
+print(rev('Gray'))
+
--- task_03.py
+++ task_03.py
@@ -1,13 +1,32 @@
 import csv
 
-carName = input('Enter the car name')
-newCar = {}
+while True:
+    car_name = input('Enter the car name')
+    new_cars = {}
+    EUR = 0.90
+    GBP = 0.81
 
-with open('items.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        items = {row['name']: {'price': {row['value'], row['currency']}}}
-        for name, price in items.items():
-            for value, currency in price.values():
-                if carName == name:
-                    print('You enter', carName, 'price for this car is:', value , currency)
+    with open('items.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        cars = {row['name']: {'price': row['value']} for row in reader}
+
+    if car_name in cars:
+        value = cars[car_name]['price']
+        print(f'You enter: {car_name} price for this car is: {value} $')
+    else:
+        currency, value = input('Pls add currency type and value: ').split()
+        try:
+            value = float(value)
+            if currency == 'EUR':
+                value *= EUR
+            elif currency == 'GBP':
+                value *= GBP
+            new_cars[car_name] = {'price': value}
+        except ValueError:
+            print('Error.Please enter number!')
+        else:
+            pass
+    if new_cars:
+        with open('items.csv','a') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['name', 'value'])
+            writer.writerow({'name': car_name,'value' : value})
